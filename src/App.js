744dc840ded1f8fa71a289a0da57715c82885d5a
PowellBrand <powellbrandon@outlook.com>
2018-02-09 14:58:53 -0700

    css updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="mainApp">
         <nav className="mainNav">
           <p className="navBtn">Helo</p>
           <p className="navBtn">home</p>
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -4,7 +4,7 @@ import './App.css';
 class Dashboard extends Component {
   render() {
     return (
-      <div>
+      <div className="mainApp">
        
       </div>
     );
